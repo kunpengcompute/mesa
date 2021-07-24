7df90b460af365443502bbcdb9605d223a3f7b4f
zhanghuaping-1 <zhanghuaping2@huawei.com>
2021-07-24 16:38:41 +0800

    open 3.0

--- src/mesa/drivers/dri/common/dri_util.c
+++ src/mesa/drivers/dri/common/dri_util.c
@@ -165,7 +165,7 @@ driCreateNewScreen2(int scrn, int fd,
     api = API_OPENGLES2;
     if (_mesa_override_gl_version_contextless(&consts, &api, &version))
        psp->max_gl_es2_version = 30;
-
+    psp->max_gl_es2_version = 30;
     api = API_OPENGL_COMPAT;
     if (_mesa_override_gl_version_contextless(&consts, &api, &version)) {
        psp->max_gl_core_version = version;
